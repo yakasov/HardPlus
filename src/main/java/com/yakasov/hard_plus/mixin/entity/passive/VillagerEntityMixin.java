750ed576cf65a7f669ee802415d301ce8ef5ed60
yakasov <yakasovian@gmail.com>
2025-08-05 22:41:53 +0100

    Maybe cap Villager trading discounts

---
+++ src/main/java/com/yakasov/hard_plus/mixin/entity/passive/VillagerEntityMixin.java
@@ -0,0 +1,20 @@
+package com.yakasov.hard_plus.mixin.entity.passive;
+
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(VillagerEntity.class)
+public class VillagerEntityMixin {
+    @Inject(
+            method = "getReputation",
+            at = @At("RETURN"),
+            cancellable = true
+    )
+    private void capReputation(PlayerEntity player, CallbackInfoReturnable<Integer> cir) {
+        cir.setReturnValue((int) Math.min(0.35, cir.getReturnValue()));
+    }
+}
